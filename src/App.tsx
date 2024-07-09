d455c6aa3f28b415a90c9af6a11d035f7bae90b9
TaejaeHan <hantaejae@gmail.com>
2024-07-09 17:34:24 +0900

    전시 데이터더미 및 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,74 @@
 import './App.css'
 import React, { useEffect, useState } from 'react';
 import Iframe from 'react-iframe'
-
-const data = {
-	urls:[
-		"https://storage.googleapis.com/tfjs-models/demos/face-detection/index.html?model=mediapipe_face_detector",
-		"https://naver.com",
-		"https://webcamtests.com/check",
-		"https://www.instagram.com/reel/C6ytN7srSiq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D",
-		"https://google.com",
-		"https://github.com/robbestad/react-iframe"
-	],
-	artist:{
-		name : [
-			"한태재",
-			"서동이",
-			"최민성",
-			"조종혁",
-			"이재상",
-			"이상헌",
-		],
-		site:[
-			'https://taejaehan.com/',
-			'https://taejaehan.com/',
-			'https://taejaehan.com/',
-			'https://taejaehan.com/',
-			'https://taejaehan.com/',
-			'https://taejaehan.com/',
-		]
-	},
-	art:{
-		title:[
-			"Truth in Pendulum",
-			"Gradient Text",
-			"Truth in Pendulum",
-			"Gradient Text",
-			"Harmonograph generator",
-			"Harmonograph generator",
-		],
-		desc:[
-			"adfvadfasdfasdfasdf",
-			"adfvadfasdfasdfasdf",
-			"adfvadfasdfasdfasdf",
-			"adfvadfasdfasdfasdf",
-			"adfvadfasdfasdfasdf",
-			"adfvadfasdfasdfasdf",
-		],
-	}
-	
-}
-
-
+import json from './data.json';
+import Countdown, { CountdownRenderProps } from 'react-countdown';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 let i = 0;
+
+function makeRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
+
+const timerRenderer = ({ hours, minutes, seconds, completed }: CountdownRenderProps) => {
+	if (completed) {
+	  // Render a completed state
+	  return <span>
+			다음작품으로 넘어갑니다
+			</span>;
+	}
+	
+	const displayTime = minutes * 50 + seconds;
+	// Render a countdown
+	return (
+	  <span>
+		{displayTime}초 후 다음작품으로 넘어갑니다
+	  </span>
+	);
+};
 function App() {
 	console.log('app')
 
-    const [nextUrl, setNextUrl] = useState<string>(data.urls[0]);
+    const [nextUrl, setNextUrl] = useState<string>(json.data[0].art.url);
+    const [nextQrImage, setNextQrImage] = useState<string>(json.data[0].artist.qr);
+    const [nextTimer, setNextTimer] = useState<number>(json.data[0].timer.milliseconds);
+    const [color, setColor] = useState<string>('rgb(0,0,0)');
 	
 	useEffect(()=>{
 		setTimeout(() => {
 			i++;
-			if(i > data.urls.length){
+			if(i > json.data.length){
 				i = 0;
 			}
-			setNextUrl(data.urls[i])
-		}, 10000);
+			setNextUrl(json.data[i].art.url)
+			setNextQrImage(json.data[i].artist.qr)
+			setNextTimer(json.data[i].timer.milliseconds)
+		}, nextTimer+2000);
 	},[nextUrl])
 
+	useEffect(()=>{
+		setTimeout(() => {
+			const col = makeRandomColor();
+			setColor(col);
+		}, 5000);
+	},[color])
+
+	console.log("nextTimer : ", nextTimer)
 	return (
 		<>
 			<Iframe url={nextUrl}
@@ -78,30 +79,91 @@ function App() {
 					className=""
 					display="block"
 					position="relative"/>
-			<div
-				style={{
-					position:'fixed',
-					bottom:0,
-					right:0,
-				}}
-			>
-				{/* {data.artist.map(
-                        (artist: any, index: number) => {
-							
-                            const title = data.art.title[index];
-                            return(
-								<>
-									<h2>{title}</h2>
-									<h2>{artist}</h2>
-								</>
-                            )
-                        },
-                    )
-                } */}
+			<div className='art-desc' >
 
-				<h2>{data.artist.name[i]}</h2>
-				<h2>{data.artist.site[i]}</h2>
-				<h2>{data.art.title[i]}</h2>
+				<TableContainer component={Paper} >
+					<Table sx={{ minWidth: 700 }} className='desc-table'>
+						<TableHead>
+							<TableRow >
+								<TableCell colSpan={5} className='countdown'>
+									<Countdown
+										key={Date.now() + nextTimer}
+										date={Date.now() + nextTimer}
+										intervalDelay={0}
+										precision={3}
+										renderer={timerRenderer}
+									/>
+								</TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell colSpan={5} className='use' style={{color:color}}>
+									{json.data[i].art.use}
+								</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							<TableRow>
+								<TableCell className='title'>
+									{json.data[i].art.title}
+								</TableCell>
+								<TableCell className='name'>
+									{json.data[i].artist.name}
+								</TableCell>
+								<TableCell className='qr'>
+									<img src={nextQrImage} alt={json.data[i].artist.name+" site"} />
+								</TableCell>
+							</TableRow>
+							
+							<TableRow>
+								<TableCell colSpan={5} className='desc'>
+									{json.data[i].art.desc}
+								</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
+				</TableContainer>
+				{/* <table className='desc-wrap'>
+					<tr>
+						<td>
+							<Countdown
+								className='countdown'
+								key={Date.now() + nextTimer}
+								date={Date.now() + nextTimer}
+								intervalDelay={0}
+								precision={3}
+								renderer={timerRenderer}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td className='title'>
+							{json.data[i].art.title}
+						</td>
+						<td className='name'>
+
+						</td>
+						<td className='qr'>
+
+						</td>
+					</tr>
+					<tr>
+						<td className='desc'>
+							{json.data[i].art.desc}
+						</td>
+					</tr>
+				</table> */}
+				{/* <h2 className='title'>
+					{json.data[i].art.title}
+				</h2>
+				<h2 className='name'>
+					{json.data[i].artist.name}
+				</h2>
+				<div className='qr'>
+					<img src={nextQrImage} alt={json.data[i].artist.name+" site"} />
+				</div>
+				<h2 className='desc'>
+					{json.data[i].art.desc}
+				</h2> */}
 			
 			</div>
 		</>
